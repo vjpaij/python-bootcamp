260f4f4d97b52bdac71dbbcfdc4c59684a2fb148
Vijay Pai <vijay.pai@tesco.com>
2024-11-03 19:20:36 +0530

    Push basic codes

---
+++ 15-Flask/flask/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+'''
+ It creates an instance of the Flask class, 
+ which will be your WSGI (Web Server Gateway Interface) application.
+'''
+###WSGI Application
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to this best Flask course.This should be an amazing course"
+
+@app.route("/index")
+def index():
+    return "Welcome to the index page"
+
+
+if __name__=="__main__":
+    #degug=true automatically rereshes the code and shows the changes. 
+    #Hence doesnt require the program to be restarted
+    app.run(debug=True)
